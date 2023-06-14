cae4225fddd3c10c78e69a62d8feb2d8a4e9f88f
rdhollinger <hol23003@byui.edu>
2023-06-14 13:03:50 -0600

    Week 9 work

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+        Rectangle s2 = new Rectangle("Green", 4, 5);
+        shapes.Add(s2);
+        Circle s3 = new Circle("Purple", 6);
+        shapes.Add(s3);
+        foreach(Shape s in shapes)
+        {
+            string color = s.getColor();
+            double area = s.getArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,19 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string getColor()
+    {
+        return _color;
+    }
+    public void setColor(string color)
+    {
+        _color = color;
+    }
+    public abstract double getArea();
+}
